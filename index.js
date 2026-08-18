9b60c8612e2632e954d2f26ffb676c5b2091877b
smyzxc <stratulenkokiril699@gmail.com>
2026-08-18 15:43:13 +0300

    feat: add initial age

---
+++ index.js
@@ -0,0 +1,10 @@
+function checkNumber(n) {
+  if (n < 0) {
+    console.log("otricalovo");
+  } else if (n > 0) {
+    console.log("polojitelnoe");
+  } else if (n === 0) {
+    console.log("ravno nulu");
+  }
+}
+checkNumber(2343223);
