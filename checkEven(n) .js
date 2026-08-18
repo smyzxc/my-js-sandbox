25732222bf15036e3025fac14a419088f405d309
smyzxc <stratulenkokiril699@gmail.com>
2026-08-18 16:18:58 +0300

    refactor: split tasks into separate files

---
+++ checkEven(n) .js
@@ -0,0 +1,9 @@
+function checkEven(n) {
+  if (n % 2 === 0) {
+    console.log("chotnoe");
+  } else {
+    console.log("nechotnoe");
+  }
+}
+
+checkEven(12334234312);
--- checkNumber(n) .js
+++ checkNumber(n) .js
@@ -7,14 +7,4 @@ function checkNumber(n) {
     console.log("ravno nulu");
   }
 }
-//checkNumber(2343223);
-
-function checkEven(n) {
-  if (n % 2 === 0) {
-    console.log("chotnoe");
-  } else {
-    console.log("nechotnoe");
-  }
-}
-
-checkEven(12334234312);
+checkNumber(2343223);
